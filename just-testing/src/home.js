0ce3b700064d3e0992a342e4d8387428f51f8cee
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-08-12 02:32:34 +0200

    outputting list in react

--- just-testing/src/home.js
+++ just-testing/src/home.js
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 
 const Home = () => {
-   const [name, setName] = useState('Maria');
-   const [age, setAge] = useState('20');
-
-   const handleClick = () =>{
-    setName('Oyindamola')
-    setAge('22')
-   }
-
+    const [blogs, setBlogs] = useState([
+       { title: 'My new website', body: 'lorem ipsum...', author: 'oyindamola', id:'1'}, 
+       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ballerina', id:'2'}, 
+       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kushimo', id:'3'}
+    ]);
+    
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            { blogs.map((blog) =>(
+                <div className='blog-preview' key={blog.id}>
+                    <h2> {blog.title} </h2>
+                    <p>Written by {blog.author} </p>
+                    </div>
+            ))}
         </div>
     );
-
 }    
 export default Home;
