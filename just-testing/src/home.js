b36422a586259cde1ab18784ee68f80aa24aafe8
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-08-12 03:20:06 +0200

    react-pages

--- just-testing/src/home.js
+++ just-testing/src/home.js
@@ -2,17 +2,18 @@ import { useState } from 'react';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-       { title: 'My new website', body: 'lorem ipsum...', author: 'oyindamola', id:'1'}, 
-       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ballerina', id:'2'}, 
-       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kushimo', id:'3'}
+       { title: 'My new website', body: 'lorem ipsum...', author: 'oyindamola', id:'1', year: '2022'}, 
+       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ballerina', id:'2', year: '2022'}, 
+       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kushimo', id:'3', year: '2022'}
     ]);
-    
+
     return (
         <div className="home">
             { blogs.map((blog) =>(
                 <div className='blog-preview' key={blog.id}>
                     <h2> {blog.title} </h2>
                     <p>Written by {blog.author} </p>
+                    <p> {blog.year} </p>
                     </div>
             ))}
         </div>
