c7b68a273379f4df74187b96f1121b1687931e68
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-08-12 03:30:57 +0200

    [UPDATE]

--- just-testing/src/home.js
+++ just-testing/src/home.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const Home = () => {
     const [blogs, setBlogs] = useState([
        { title: 'My new website', body: 'lorem ipsum...', author: 'oyindamola', id:'1', year: '2022'}, 
-       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ballerina', id:'2', year: '2018'}, 
+       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ballerina', id:'2', year: '2017'}, 
        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kushimo', id:'3', year: '2000'}
     ]);
 
